b0cfdf08a54413cd0f4a36bbe71fcb943d8659a3
JohnnyMoney <wyndrockjohn@gmail.com>
2026-02-15 07:58:41 -0500

    Add files via upload

---
+++ pages/index.js
@@ -0,0 +1,57 @@
+import { v4 as uuidv4 } from "https://jspm.dev/uuid";
+import { initialTodos, validationConfig } from "../utils/constants.js";
+import  Todo  from "../components/Todo.js"; 
+import FormValidator from "../components/FormValidator.js";
+
+
+const addTodoButton = document.querySelector(".button_action_add");
+const addTodoPopup = document.querySelector("#add-todo-popup");
+const addTodoForm = document.forms["add-todo-form"];
+const addTodoCloseBtn = addTodoPopup.querySelector(".popup__close");
+const todoTemplate = document.querySelector("#todo-template");
+const todosList = document.querySelector(".todos__list");
+
+const openModal = (modal) => {
+  modal.classList.add("popup_visible");
+};
+
+const closeModal = (modal) => {
+  modal.classList.remove("popup_visible");
+};
+
+
+const generateTodo = (data) => {
+  const todo = new Todo(data, "#todo-template");
+  const todoElement = todo.getView();
+  return todoElement;
+};
+
+const formValidator = new FormValidator(validationConfig, addTodoForm);
+formValidator.enableValidation();
+
+addTodoButton.addEventListener("click", () => {
+  openModal(addTodoPopup);
+});
+
+addTodoCloseBtn.addEventListener("click", () => {
+  closeModal(addTodoPopup);
+});
+
+addTodoForm.addEventListener("submit", (evt) => {
+  evt.preventDefault();
+  const name = evt.target.name.value;
+  const dateInput = evt.target.date.value;
+
+  const date = new Date(dateInput);
+  date.setMinutes(date.getMinutes() + date.getTimezoneOffset());
+
+const id = uuidv4();
+const values = { name, date, id };
+renderTodo(values);
+closeModal(addTodoPopup);
+  closeModal(addTodoPopup);
+});
+
+initialTodos.forEach((item) => {
+  renderTodo(item);
+});
